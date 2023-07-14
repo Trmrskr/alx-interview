1bc43f05718a4f7a09699e8d3d14da7e678297c0
Trmrskr <simonalale@gmail.com>
2023-07-14 14:04:23 +0000

    Major correction

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -17,7 +17,16 @@ def minOperations(n):
 
     operations = 0
     i = 0
-    prime_number_determinants = [2, 3, 5, 7]
+    prime_number_determinants =
+    [
+        2, 3, 5, 7, 11, 13, 17, 19, 23,
+        29, 31, 37, 41, 43, 47, 53, 59,
+        61, 67, 71, 73, 79, 83, 89, 97,
+        101, 103, 107, 109, 113, 127, 131,
+        137, 139, 149, 151, 157, 163, 167,
+        173, 179, 181, 191, 193, 197, 199
+    ]
+
     size = len(prime_number_determinants)
 
     while i < size:
