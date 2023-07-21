20abe8e3ba2c28664723465104b9ca264ef2db02
Trmrskr <simonalale@gmail.com>
2023-07-20 19:14:38 -0700

    Optimization

--- 0x03-log_parsing/0-stats_cp.py
+++ 0x03-log_parsing/0-stats_cp.py
@@ -16,8 +16,8 @@ def print_status(status_code, size):
 
 def log_stat():
     """Compute the log statistics"""
-    status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
-                   "403": 0, "404": 0, "405": 0, "500": 0}
+    status_code = {200: 0, 301: 0, 400: 0, 401: 0,
+                   403: 0, 404: 0, 405: 0, 500: 0}
     count = 0
     total_file_size = 0
 
@@ -33,14 +33,16 @@ def log_stat():
 
             words = line.split(" ")
             total_file_size += int(words[-1])
-            code = words[-2]
+            code = int(words[-2])
             if code in status_code:
                 status_code[code] += 1
 
+        print_status(status_code, total_file_size)
     except KeyboardInterrupt:
         raise
     finally:
         print_status(status_code, total_file_size)
+
 
 
 if __name__ == "__main__":
