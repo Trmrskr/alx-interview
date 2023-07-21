a5ec27bcada01ff6674f7b77854f3ba046489b82
Trmrskr <simonalale@gmail.com>
2023-07-20 19:49:20 -0700

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,7 +5,7 @@
 import sys
 
 
-def printStatus(dic, size):
+def print_status(dic, size):
     """ Prints information """
     print("File size: {:d}".format(size))
     for i in sorted(dic.keys()):
@@ -13,34 +13,40 @@ def printStatus(dic, size):
             print("{}: {:d}".format(i, dic[i]))
 
 
-# sourcery skip: use-contextlib-suppress
-statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0}
+def log_stat():
+    # sourcery skip: use-contextlib-suppress
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                    "404": 0, "405": 0, "500": 0}
 
-count = 0
-size = 0
+    count = 0
+    size = 0
 
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printStatus(statusCodes, size)
+    try:
+        for line in sys.stdin:
+            if count != 0 and count % 10 == 0:
+                print_status(status_codes, size)
 
-        stlist = line.split()
-        count += 1
+            words = line.split()
+            count += 1
 
-        try:
-            size += int(stlist[-1])
-        except Exception:
-            pass
+            try:
+                size += int(words[-1])
+            except Exception:
+                pass
 
-        try:
-            if stlist[-2] in statusCodes:
-                statusCodes[stlist[-2]] += 1
-        except Exception:
-            pass
-    printStatus(statusCodes, size)
+            try:
+                if words[-2] in status_codes:
+                    status_codes[words[-2]] += 1
+            except Exception:
+                pass
+        print_status(status_codes, size)
 
 
-except KeyboardInterrupt:
-    printStatus(statusCodes, size)
-    raise
+    except KeyboardInterrupt:
+        print_status(status_codes, size)
+        raise
+
+
+if __name__ == "__main__":
+    """main namespace"""
+    log_stat()
