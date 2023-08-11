9c559643850463b225b5273fcea6a5a359d91880
Trmrskr <simonalale@gmail.com>
2023-08-11 07:21:18 -0700

    Star wars API: semistandard code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,19 +4,18 @@ const request = require('request');
 const argument = process.argv[2];
 const filmsUrl = 'https://swapi-api.alx-tools.com/api/films/' + argument;
 
-const print_actor_names = (idx, actors) => {
+const printActorNames = (idx, actors) => {
   if (idx === actors.length) return;
   request(actors[idx], (error, response, body) => {
     if (error) throw error;
     console.log(JSON.parse(body).name);
-    print_actor_names(++idx, actors);
+    printActorNames(++idx, actors);
   });
-}
-
+};
 request(filmsUrl, (err, response, body) => {
   if (err) throw err;
   const actors = JSON.parse(body).characters;
-  print_actor_names(0, actors);
+  printActorNames(0, actors);
 });
 
 
