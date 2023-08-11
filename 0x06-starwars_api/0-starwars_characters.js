f81c3c705ba6c66ece79cd0dce3ff3ef62afa670
Trmrskr <simonalale@gmail.com>
2023-08-11 07:11:00 -0700

    Starwars Characters redone

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const argument = process.argv[2];
+const filmsUrl = 'https://swapi-api.alx-tools.com/api/films/' + argument;
 
 const print_actor_names = (idx, actors) => {
-  if (idx === actors.length)
-    return;
+  if (idx === actors.length) return;
 
   request(actors[idx], (error, response, body) => {
-    if (error)
-      throw error;
+    if (error) throw error;
+
     console.log(JSON.parse(body).name);
+    print_actor_names(++idx, actors);
   });
-  print_actor_names(idx + 1, actors);
 }
 
-let filmsUrl = 'https://swapi-api.alx-tools.com/api/films/' + argument + '/';
-
 request(filmsUrl, (err, response, body) => {
-  if (err)
-    throw err;
+  if (err) throw err;
 
-  actors = JSON.parse(body).characters;
+  const actors = JSON.parse(body).characters;
   print_actor_names(0, actors);
 });
 
