428ca88ed03154280de64cc03c433748419c09d4
Trmrskr <simonalale@gmail.com>
2023-08-11 07:13:17 -0700

    Starwars Characters semistandard correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,10 +6,8 @@ const filmsUrl = 'https://swapi-api.alx-tools.com/api/films/' + argument;
 
 const print_actor_names = (idx, actors) => {
   if (idx === actors.length) return;
-
   request(actors[idx], (error, response, body) => {
     if (error) throw error;
-
     console.log(JSON.parse(body).name);
     print_actor_names(++idx, actors);
   });
@@ -17,7 +15,6 @@ const print_actor_names = (idx, actors) => {
 
 request(filmsUrl, (err, response, body) => {
   if (err) throw err;
-
   const actors = JSON.parse(body).characters;
   print_actor_names(0, actors);
 });
