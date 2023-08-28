b2b1ec7fc97f211e9dea4525bbdedfb4fd448647
Trmrskr <simonalale@gmail.com>
2023-08-28 09:47:11 -0700

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This script solves the Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """function that solves the island perimeter problem"""
+
+    def dfs(i, j):
+        """Performs depth first search"""
+        if grid[i][j] == 0 or i < 0 or j < 0 or i >= len(grid) \
+                or j >= len(grid[0]):
+            return 1
+        if grid[i][j] == 1:
+            grid[i][j] = 2
+            return dfs(i - 1, j) + dfs(i, j - 1) + dfs(i + 1, j) + \
+                dfs(i, j + 1)
+        return 0
+
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[0])):
+            if (grid[i][j]):
+                return dfs(i, j)
